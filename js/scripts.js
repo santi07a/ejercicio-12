34f4d219e3fed6e171fe52796327be6e25bd69ea
Santi Arnau <santi07a@gmail.com>
2021-03-03 18:59:09 +0100

    peticion api

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+const urlFacturas = "http://localhost:3001/facturas";
+
+let facturas;
+
+const apiFacturas = async () => {
+  const respuesta = await fetch(urlFacturas);
+  facturas = await respuesta.json();
+};
