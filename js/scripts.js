9b3b87c01be6d971a9348150c0bc0f74688378e4
Santi Arnau <santi07a@gmail.com>
2021-03-03 22:48:01 +0100

    creacion de text contents para molde

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,29 @@
 const urlFacturas = "http://localhost:3001/facturas";
-const numeroFactura = document.querySelector(".numero-factura");
-const fechaFactura = document.querySelector(".fecha-factura");
-const aplicacion = document.querySelector(".aplicacion");
-const montoBase = document.querySelector(".monto-base");
-const montoIva = document.querySelector(".monto-iva");
-const montoTotal = document.querySelector(".monto-total");
-const estadoFactura = document.querySelector(".estado-factura");
-const facturaVence = document.querySelector(".factura-vence");
-
+const listado = document.querySelector(".lista-facturas");
 let facturas;
 
 const apiFacturas = async () => {
   const respuesta = await fetch(urlFacturas);
   facturas = await respuesta.json();
+  const nuevaFila = document.querySelector(".dummy").cloneNode(true);
+  nuevaFila.classList.remove("dummy");
+  nuevaFila.hidden = false;
+  const facturasIngreso = facturas
+    .filter(factura => factura.tipo === "ingreso");
+  console.log(facturasIngreso);
+  for (const factura of facturasIngreso) {
+    nuevaFila.querySelector(".numero-factura").textContent = factura.numero;
+    nuevaFila.querySelector(".fecha-factura").textContent = factura.fecha;
+    nuevaFila.querySelector(".aplicacion").textContent = factura.concepto;
+    nuevaFila.querySelector(".monto-base").textContent = factura.base;
+    nuevaFila.querySelector(".monto-iva").textContent = calculaIva(factura.base);
+    nuevaFila.querySelector(".monto-total").textContent = factura.base + calculaIva(factura.base);
+    nuevaFila.querySelector(".estado-factura").textContent = 0;
+    nuevaFila.querySelector(".factura-vence").textContent = 0;
+
+    listado.append(nuevaFila);
+  }
 };
 apiFacturas();
+
+const calculaIva = numero => (numero * 21) / 100;
